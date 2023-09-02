2447df28e4370e965bd81e689dc58135382d2e91
Liza0806 <126647343+Liza0806@users.noreply.github.com>
2023-09-02 09:15:19 +0300

    k

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,10 +5,10 @@ import user from '../data/user.json';
 import { StatsBoard } from './statistics/StatisticsBoard';
 import data from '../data/data.json';
 
-import { FriendList } from 'components/friendlist/FriendList';
+import { FriendList } from './friendlist/FriendList';
 import friends from '../data/friends.json'
 
-import { TransactionHistory } from "components/transaction/Transaction";
+import { TransactionHistory } from "./transaction/Transaction";
 import transactions from '../data/transactions.json';
 
 import { Container } from './App.styled.js';
